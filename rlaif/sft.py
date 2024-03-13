aa69b013eb5e398fa8c586c97e72ef5c6f0a5574
Andrew Silva <asilvaa9@gmail.com>
2024-03-13 17:51:38 -0600

    remove print

--- rlaif/sft.py
+++ rlaif/sft.py
@@ -252,7 +252,6 @@ def train(mdl, train_ds, val_set, optimizer, loss_fn, tok, train_args):
         # Model update
         optimizer.update(mdl, grad)
         mx.eval(mdl.parameters(), optimizer.state, lvalue)
-        print(lvalue.item())
 
         # Record loss
         losses.append(lvalue.item())
